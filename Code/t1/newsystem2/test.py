97f98804a4733421f44d03e1698207de031fdc12
Pranav Talluri <pranav.talluri.pt@gmail.com>
2022-12-21 23:24:15 +0000

    Create test slurm

--- Code/t1/newsystem2/test.py
+++ Code/t1/newsystem2/test.py
@@ -1,12 +1,35 @@
+import functools
+from typing import List
+
 import torch
-from torch import Tensor
+import torch.utils.data
+import torchvision
+from torchvision.utils import save_image as imsave
+import torchvision.transforms as tvt
+import os
+from tqdm.auto import tqdm
 
-mean1, logvar1, mean2, logvar2 = Tensor([1]), Tensor([2]), Tensor([3]), Tensor([4])
+from ddpm import DDPM
+from unet import UNet
+import torch
+from torch import nn, Tensor
+from torch.nn import Identity, Module, ModuleList, Linear, Sigmoid, GroupNorm, Conv2d, ConvTranspose2d, Dropout
+from typing import Union, Tuple, List, Optional, Callable
+import enum
+from tqdm.auto import tqdm
+from utils import normal_kl, mean_flat, discretized_gaussian_log_likelihood, gather, extract
+import numpy as np
+import torchvision as tv
+import torchvision.transforms.functional as fn
+from scipy import linalg
 
-t = None
-for obj in (mean1, logvar1, mean2, logvar2):
-    if isinstance(obj, Tensor):
-        t = obj
-        break
+import math
+from typing import Union, Tuple, List
 
-logvar1, logvar2 = [x if isinstance(x, Tensor) else torch.tensor(x).to(t) for x in (logvar1, logvar2)]
+import torch
+from torch import nn, Tensor
+from torch.nn import Identity, Module, ModuleList, Linear, Sigmoid, GroupNorm, Conv2d, ConvTranspose2d, Dropout
+
+dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("running test script")
+print(dev)
